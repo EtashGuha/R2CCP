a91c42aa5cc701b84abdf820b8ffd17324371e29
EtashGuha <etashthebomb@gmail.com>
2024-03-27 02:14:34 -0400

    Update cp.py

--- R2CCP/cp.py
+++ R2CCP/cp.py
@@ -37,22 +37,20 @@ def find_intervals_above_value_with_interpolation(x_values, y_values, cutoff):
     
 def get_all_scores(range_vals, X, y, model):
     step_val = (max(range_vals) - min(range_vals))/(len(range_vals) - 1)
-    indices_up = torch.ceil((y - min(range_vals))/step_val)
-    indices_down = torch.floor((y - min(range_vals))/step_val)
+    indices_up = torch.ceil((y - min(range_vals))/step_val).squeeze()
+    indices_down = torch.floor((y - min(range_vals))/step_val).squeeze()
     
-    how_much_each_direction = (y - min(range_vals))/step_val - indices_down
+    how_much_each_direction = ((y.squeeze() - min(range_vals))/step_val - indices_down)
 
     weight_up = how_much_each_direction
     weight_down = 1 - how_much_each_direction
 
-    bad_indices = torch.where(torch.logical_or(y > max(range_vals), y < min(range_vals)))
+    bad_indices = torch.where(torch.logical_or(y.squeeze() > max(range_vals), y.squeeze() < min(range_vals)))
     indices_up[bad_indices] = 0
     indices_down[bad_indices] = 0
     
     scores = get_scores(X, model)
-
-    all_scores = scores[torch.arange(len(X)), indices_up.squeeze().long()] * weight_up + scores[torch.arange(len(X)), indices_down.squeeze().long()] * weight_down
-    
+    all_scores = scores[torch.arange(len(X)), indices_up.long()] * weight_up + scores[torch.arange(len(X)), indices_down.long()] * weight_down
     all_scores[bad_indices] = 0
     return scores, all_scores
 
